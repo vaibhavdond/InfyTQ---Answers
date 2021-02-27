9bb9b51777c46969d8f89e547f830ccc595ff8cf
vaibhavdond <60029073+vaibhavdond@users.noreply.github.com>
2021-02-27 13:54:25 +0530

    Initial Commit
    Exercise 13

---
+++ Programming Fundamentals using Python/Day 2/Exercise 14.py
@@ -0,0 +1,13 @@
+#Exercise-14
+
+grade = "Z"
+marks= 63
+if(marks>=0 and marks<=64):
+    grade="D"
+elif(marks>=65 and marks<=72):
+    grade="C"
+elif(marks>=73 and marks<=79):
+    grade="B"
+elif(marks>=80 and marks<=100):
+    grade="A"
+print(grade)
